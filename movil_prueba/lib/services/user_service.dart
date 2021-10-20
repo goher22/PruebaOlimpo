4cb7140c88fd306c280777abac34aaff5b982693
Carlos <gohercarlos@gmail.com>
2021-10-20 03:00:12 -0500

    Cambio en el servicion de los usuarios

--- movil_prueba/lib/services/user_service.dart
+++ movil_prueba/lib/services/user_service.dart
@@ -8,31 +8,31 @@ class UsersServices extends ChangeNotifier {
   final String _baseUrl = 'https://prueba-olimpo-default-rtdb.firebaseio.com';
   bool isSaving = false;
 
-  Future saveOrCreateProduct(User user) async {
+  Future saveOrCreateUser(User user) async {
     isSaving = true;
     notifyListeners();
 
     if (user.id == null) {
       // Es necesario crear
-      await this.createProduct(user);
+      await this.createUser(user);
     } else {
       // Actualizar
-      //await this.updateProduct( product );
+      //await this.updateUser( product );
     }
 
     isSaving = false;
     notifyListeners();
   }
 
-  Future<String> createProduct(User user) async {
-    final url = Uri.https(_baseUrl, 'products.json');
+  Future<String> createUser(User user) async {
+    final url = Uri.https(_baseUrl, 'users.json');
     final resp = await http.post(url, body: user.toJson());
     final decodedData = json.decode(resp.body);
 
     user.id = decodedData['name'];
 
     //TODO: Editar listado de usuario
-    //this.users.add(product);
+    //this.users.add(user);
 
     return user.id!;
   }
