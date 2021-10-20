e1c886050473c858699ce28da61805965e6298c7
Carlos <gohercarlos@gmail.com>
2021-10-20 03:20:47 -0500

    Pantall 1 completado

--- movil_prueba/lib/screens/captur_data_screen.dart
+++ movil_prueba/lib/screens/captur_data_screen.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:movil_prueba/providers/users_provider.dart';
 import 'package:movil_prueba/ui/input_decorations.dart';
+import 'package:provider/provider.dart';
 
 class CapturDataScreen extends StatelessWidget {
   const CapturDataScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final userProvider = Provider.of<UsersProvider>(context);
+    final user = userProvider.user!;
     return Scaffold(
       appBar: AppBar(
         title: Text('Registro de usuarios'),
       ),
       body: SingleChildScrollView(
         child: Form(
+          key: userProvider.formKey,
           child: Container(
             padding: EdgeInsets.symmetric(horizontal: 30),
             child: Column(
               children: <Widget>[
                 SizedBox(height: 10),
                 TextFormField(
+                  initialValue: user.name,
+                  onChanged: (value) => user.name = value,
+                  validator: (value) {
+                    if (value == null || value.length < 1)
+                      return 'El nombre es obligatorio';
+                  },
                   decoration: InputDecorations.authInputDecoration(
                     hintText: 'Nombre de usuario',
                     labelText: 'Nombre:',
@@ -25,6 +36,12 @@ class CapturDataScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 30),
                 TextFormField(
+                  initialValue: user.iCard,
+                  onChanged: (value) => user.iCard = value,
+                  validator: (value) {
+                    if (value == null || value.length < 1)
+                      return 'La cedula es obligatorio';
+                  },
                   decoration: InputDecorations.authInputDecoration(
                     hintText: 'Cedula de usuario',
                     labelText: 'Cedula:',
@@ -32,6 +49,12 @@ class CapturDataScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 30),
                 TextFormField(
+                  initialValue: user.address,
+                  onChanged: (value) => user.address = value,
+                  validator: (value) {
+                    if (value == null || value.length < 1)
+                      return 'La dirección es obligatorio';
+                  },
                   decoration: InputDecorations.authInputDecoration(
                     hintText: 'Dirección de usuario',
                     labelText: 'Dirección:',
@@ -39,6 +62,12 @@ class CapturDataScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 30),
                 TextFormField(
+                  initialValue: user.city,
+                  onChanged: (value) => user.city = value,
+                  validator: (value) {
+                    if (value == null || value.length < 1)
+                      return 'La ciudad es obligatorio';
+                  },
                   decoration: InputDecorations.authInputDecoration(
                     hintText: 'Ciudad de usuario',
                     labelText: 'Ciudad:',
@@ -46,6 +75,12 @@ class CapturDataScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 30),
                 TextFormField(
+                  initialValue: user.country,
+                  onChanged: (value) => user.country = value,
+                  validator: (value) {
+                    if (value == null || value.length < 1)
+                      return 'El país es obligatorio';
+                  },
                   decoration: InputDecorations.authInputDecoration(
                     hintText: 'País de usuario',
                     labelText: 'País:',
@@ -53,6 +88,12 @@ class CapturDataScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 30),
                 TextFormField(
+                  initialValue: user.mobile,
+                  onChanged: (value) => user.mobile = value,
+                  validator: (value) {
+                    if (value == null || value.length < 1)
+                      return 'El celular es obligatorio';
+                  },
                   decoration: InputDecorations.authInputDecoration(
                     hintText: 'Celular de usuario',
                     labelText: 'Celular:',
@@ -67,6 +108,7 @@ class CapturDataScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.navigate_next),
         onPressed: () {
+          if (!userProvider.isValidForm()) return;
           Navigator.pushNamed(context, 'take');
         },
       ),
